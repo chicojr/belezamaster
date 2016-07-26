6dfc5c08e2bf8dea25e40bb8979ba1341eb8ffda
Victor <vt050219@gmail.com>
2016-07-26 16:58:01 -0300

    Cadastro Profissionais ok,Perfect!!!!!   Alunos: MIKHAEL E victor

---
+++ BelezaMaster/src/br/com/ifpe/belezamaster/model/Profissional.java
@@ -0,0 +1,64 @@
+package br.com.ifpe.belezamaster.model;
+
+public class Profissional {
+private	int id;
+private String nome;
+private String profissao;
+private String cpf;
+private String email;
+private String endereco;
+private String telefone;
+private String celular;
+
+//Get e Sets!
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getProfissao() {
+	return profissao;
+}
+public void setProfissao(String profissao) {
+	this.profissao = profissao;
+}
+public String getCpf() {
+	return cpf;
+}
+public void setCpf(String cpf) {
+	this.cpf = cpf;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getEndereco() {
+	return endereco;
+}
+public void setEndereco(String endereco) {
+	this.endereco = endereco;
+}
+public String getTelefone() {
+	return telefone;
+}
+public void setTelefone(String telefone) {
+	this.telefone = telefone;
+}
+public String getCelular() {
+	return celular;
+}
+public void setCelular(String celular) {
+	this.celular = celular;
+}
+
+}
